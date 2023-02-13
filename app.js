309c7694d336e64e9d1f86f4e09b1eb3a7055a8f
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-02-12 22:30:10 -0800

    finish complete and todo

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var checkBox_in_to_do = todo.querySelectorAll("input[type=checkbox]");
 //checkbox in complete
 var checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
 
-console.log(checkbox_in_completed)
 //Define Varible
 let input_text_Value = null;
 
@@ -19,6 +18,7 @@ let input_text_Value = null;
 
 function input_text_Value_assign(){
    input_text_Value =  input_text.value;
+   console.log(input_text_Value)
 }
 function toIncomplete() {
   if (input_text_Value != null) {
@@ -26,36 +26,52 @@ function toIncomplete() {
     input_text_Value = null
     input_text.value = ''
 }
-//add next slibling ele value to incomplete2
 
 }
+
+//add next slibling ele value to incomplete2
+function toIncomplete2(val){
+    todo.innerHTML += `<li><input type="checkbox"><label>${val}</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>`;
+}
+//add next slibing ele value to complete
 function toComplete(val){
     completed.innerHTML += `<li><input type="checkbox" checked><label>${val}</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>`
 }
-//check incompleted
-function Check_completed(){
+
+function Check_Incompleted(){
+    checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
+
     checkbox_in_completed.forEach((ele)=>{
         ele.addEventListener('click',(e)=>{
+            console.log(ele.nextElementSibling.innerHTML)
             if(ele.checked == false){
                 completed.removeChild(ele.parentElement)
+                toIncomplete2(ele.nextElementSibling.innerHTML)
+                
+                    toIncomplete();
+                    
+                    checkBox_in_to_do = todo.querySelectorAll("input[type=checkbox]");
+                    checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
+                
+                    checkBox_in_to_do.forEach((ele)=>{
+                        ele.addEventListener('click',(e)=>{
+                            console.log(e.target.checked)
+                            if(e.target.checked == true){
+                                toComplete(e.target.nextElementSibling.innerHTML)
+                                todo.removeChild( e.target.parentElement)
+                                Check_Incompleted()
+                                
+                            }
+                        })
+                    })
+                  
+                
             }
         })
     })
 }
-function Check_in_to_do(){
-    //when checked in todo , list fall down to completed
-checkBox_in_to_do.forEach((ele)=>{
-    ele.addEventListener('click',(e)=>{
-        if(e.target.checked == true){
-            toComplete(e.target.nextElementSibling.innerHTML)
-            todo.removeChild( e.target.parentElement)
-            checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
-            Check_completed()
-        }
-    })
-})
-}
-//function check
+
+
 
 //Event Setup
 
@@ -65,12 +81,36 @@ input_text.addEventListener('keydown',input_text_Value_assign)
 //add list to todolist tag when add button click
 addBtn.addEventListener('click',()=>{
     toIncomplete();
+    
     checkBox_in_to_do = todo.querySelectorAll("input[type=checkbox]");
     checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
 
-    Check_in_to_do()
+    checkBox_in_to_do.forEach((ele)=>{
+        ele.addEventListener('click',(e)=>{
+            console.log(e.target.checked)
+            if(e.target.checked == true){
+                toComplete(e.target.nextElementSibling.innerHTML)
+                todo.removeChild( e.target.parentElement)
+                Check_Incompleted()
+                
+            }
+        })
+    })
+  
 })
 
-Check_completed()
-Check_in_to_do()
+//when checked in todo , list fall down to completed
+checkBox_in_to_do.forEach((ele)=>{
+    ele.addEventListener('click',(e)=>{
+        console.log(e.target.checked)
+        if(e.target.checked == true){
+            toComplete(e.target.nextElementSibling.innerHTML)
+            todo.removeChild( e.target.parentElement)
+            checkbox_in_completed = completed.querySelectorAll('input[type=checkbox]');
+            Check_Incompleted()
+        }
+    })
+})
 
+
+// const editBtn = document.querySelector('.edit')
